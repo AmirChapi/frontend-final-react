05c16d719356e0cdd77bfb919d8b3ce2ca020067
amir chapi <amirmalka26@gmail.comgit config --global user.name amir>
2025-03-25 22:21:18 +0200

    test 3

--- code/src/components/MSGForms.jsx
+++ code/src/components/MSGForms.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-export default function MSGFForms() {
+export default function MSGFFormsa() {
   return (
     <Box
       component="form"
